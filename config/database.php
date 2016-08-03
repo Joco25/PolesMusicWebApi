3158fbc7d14a3b027b51e391ed27d50184e22b81
Abati Adewale <acekyd01@gmail.com>
2016-08-03 13:18:38 +0100

    db

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'http://http://sql8.freesqldatabase.com/'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sql8129892'),
+            'username' => env('DB_USERNAME', 'sql8129892'),
+            'password' => env('DB_PASSWORD', 'qeD8F9Zf7b'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
